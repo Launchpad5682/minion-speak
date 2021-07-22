a6f861a94f04413b80b020977cd68a232f739380
launchpad5682 <launchpad5682@gmail.com>
2021-07-23 00:16:37 +0530

    script updated

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,25 @@ function getTranslationURL(sentence) {
   return serverURL + "?" + "text=" + sentence;
 }
 
-function errorHandler(){
-    
+function errorHandler(error) {
+  console.log("Error occurred: ", error);
+  alert("Something went wrong! Try Again in some time");
 }
+
+function updateOutput(data) {
+  let translatedText = data.contents.translated;
+  output.innerHTML = translatedText;
+  textArea.value = "";
+}
+
+function clickHandler() {
+  let sentence = textArea.value;
+
+  // fetching api and function reference
+  fetch(getTranslationURL(sentence))
+    .then((res) => res.json())
+    .then(updateOutput)
+    .catch(errorHandler);
+}
+
+btn.addEventListener("click", clickHandler);
